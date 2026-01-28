35de213a7b27f8257a7e1270955fe6f71af717f0
Sude Ates <sdnrates2003@gmail.com>
2026-01-28 20:22:05 +0300

    Add bubble sort algorithm implementation in C

---
+++ Ders3/bubblesort.c
@@ -0,0 +1,15 @@
+//bubble sort algorithm in C
+#include <stdio.h>
+void bubbleSort(int arr[], int n) {
+    int i, j, temp;
+    for (i = 0; i < n-1; i++) {
+        for (j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                // Swap arr[j] and arr[j+1]
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
